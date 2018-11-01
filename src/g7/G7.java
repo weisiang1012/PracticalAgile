0784ddab0e780753185196fc59844cf3d3af2a45
Tarc <Tarc@B113>
2018-11-01 10:44:48 +0800

    asd

--- src/g7/G7.java
+++ src/g7/G7.java
@@ -17,6 +17,9 @@ public class G7 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World 1");
+        System.out.println("Hello World 2");
+        System.out.println("Hello World 3");
+        System.out.println("Hello World 4");
     }
     
 }
